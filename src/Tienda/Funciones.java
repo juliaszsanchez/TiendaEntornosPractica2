d7e52e30415ab1d35a5e7cdbd1203dff7b05d754
noahzaemm <159116800+noahzaemm@users.noreply.github.com>
2024-03-28 17:59:07 +0100

    Update noah Funciones.java

--- src/Tienda/Funciones.java
+++ src/Tienda/Funciones.java
@@ -5,6 +5,9 @@ public class Funciones {
 	public static Producto[] rellenarProducto() {
 
 		Producto ListaProductos[] = new Producto[22];
+
+
+		String mod  = "noah mod";
 //CONFLICTO ARREGLADO
 		ListaProductos[0] = new Producto("Altavoz", 89.99, false);
 		ListaProductos[1] = new Producto("Auriculares Alambricos", 53.99, false);
